0a2c2e2f7756c2225bb67f32b5fbf824a6772593
hsingpu <1927833039@qq.com>
2022-03-01 09:39:07 +0800

    增加total kfk process

--- tele-exporter/kfk_exporter.py
+++ tele-exporter/kfk_exporter.py
@@ -6,6 +6,12 @@ import time
 # 定义数据类型，metric，describe(描述)，标签
 
 kfk_process = Gauge('kfk_process', '状态: kafka进程是否存在 ', ['instance'])
+total_kfk_process = Gauge('total_kfk_process', '状态: kafka进程总计 ', ['instance'])
+
+# 获取主机ip
+f = os.popen("hostname -i  ")
+ip = f.read().strip('\n')
+f.close()
 
 
 def get_kfk_process():
@@ -16,12 +22,20 @@ def get_kfk_process():
     if broker:
         state = 1
     broker.close()
-    # 获取主机ip
-    f = os.popen("hostname -i  ")
-    ip = f.read().strip('\n')
-    f.close()
     print('获取信息%s,%s' % (ip, state))
     kfk_process.labels(instance=ip).set(state)
+
+
+def get_total_kfk_process():
+    total_kfk_process.labels(instance=ip).set(3)  # DEBUG
+
+
+def get_kfk_info():
+    '''
+    获取topic等信息
+    :return:
+    '''
+    pass
 
 
 if __name__ == "__main__":
@@ -30,4 +44,5 @@ if __name__ == "__main__":
     # 不断传入数据
     while True:
         get_kfk_process()
+        get_total_kfk_process()
         time.sleep(1)
