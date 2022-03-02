c03099348d8604ee7f5511825deed9d5e7cde5e9
hsingpu <1927833039@qq.com>
2022-03-02 16:41:40 +0800

    no message

--- kfkCheck/main.py
+++ kfkCheck/main.py
@@ -1,15 +1,15 @@
 import os
 
-# kafka 路径  topic名称  kafka地址端口
-# kfkdir = '/data/kafka/kafka01/'
-# topicname = 'quickstart-events04'
-# bootstrap = '133.0.124.212:39092'
-# command = r"%sbin/kafka-topics.sh --describe --topic %s --bootstrap-server %s | grep -v Configs | awk -F ' ' '{print $6}'" % (
-#     kfkdir, topicname, bootstrap)
-#
-# # print(command)
-# commandres = os.popen(command)
-# res = commandres.read().rstrip('\n')
+kafka 路径  topic名称  kafka地址端口
+kfkdir = '/data/kafka/kafka01/'
+topicname = 'quickstart-events04'
+bootstrap = '133.0.124.212:39092'
+command = r"%sbin/kafka-topics.sh --describe --topic %s --bootstrap-server %s | grep -v Configs | awk -F ' ' '{print $6}'" % (
+    kfkdir, topicname, bootstrap)
+
+# print(command)
+commandres = os.popen(command)
+res = commandres.read().rstrip('\n')
 
 
 
