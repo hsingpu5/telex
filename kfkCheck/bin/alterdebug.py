069aab44163f279fc3ed399c075655fc9d8a231c
hsingpu <1927833039@qq.com>
2022-03-03 12:49:33 +0800

    报警脚本

---
+++ kfkCheck/bin/alterdebug.py
@@ -0,0 +1,35 @@
+#!/bin/bash/env python3
+
+import requests  # pip3 install requests -i https://pypi.douban.com/simple
+import json
+
+headers = {
+    'content-type': 'application/json',
+}
+
+url = 'http://133.0.206.49:9093/api/v1/alerts'
+
+
+def alterinfo(ip, summary, description, paasServiceName="CRM3-AMC"):
+    data = [
+        {
+            "labels": {
+                "instance": ip,
+                "serverity": "warning",
+                "cluster": "amc",
+                "alertType": "amc_change",
+                "alertname": "amc_alert",
+                "paasServiceName": paasServiceName
+            },
+            "annotations": {
+                "description": description,
+                "summary": summary
+            }
+        }
+    ]
+    print('组内容',paasServiceName)
+    #response = requests.post(url, headers=headers, json=data)
+    #print(response.content)
+    #return response.content
+
+alterinfo('ip地址','描述','详述',paasServiceName='kfk')
