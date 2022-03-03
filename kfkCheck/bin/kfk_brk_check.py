2023a043156cca64f168b65bb487b8e68e4ae441
hsingpu <1927833039@qq.com>
2022-03-03 12:55:05 +0800

    debug完毕

--- kfkCheck/bin/kfk_brk_check.py
+++ kfkCheck/bin/kfk_brk_check.py
@@ -8,7 +8,7 @@ ret = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))
 
 sys.path.insert(0, ret)
 
-#from kfkCheck.bin.alter import alterinfo
+# from kfkCheck.bin.alter import alterinfo
 
 debug = False
 # debug=True
@@ -21,29 +21,25 @@ kfkdir = '/app/ofcs/kafka/kafka-server/'
 topicname = 'plca_group_yc_lte3_topic_4'
 bootstrap = '133.0.124.212:39092'
 zookeeper = "133.0.208.212:2186,133.0.208.213:2186,133.0.208.214:2186"
-brkid={1,2,3,4,5,6}
-brkid={1,2,3,4,5,6,7}  #debug
+
+brkid = {1, 2, 3, 4, 5, 6}
+brkid = {1, 2, 3, 4, 5, 6, 7}  # debug
 if not debug:
     command = r"%sbin/kafka-topics.sh --describe --topic %s --zookeeper %s | grep -v Configs | awk -F ' ' '{print $10}' | sort | uniq" % (
         kfkdir, topicname, zookeeper)
-    print(command)
+
 else:
     command = r"%sbin/kafka-topics.sh --describe --topic %s --bootstrap-server %s | grep -v Configs | awk -F ' ' '{print $6}'" % (
         kfkdir, topicname, bootstrap)
-    print(command)
-# print(command)
+
+print(command)
 commandres = os.popen(command)
 res = commandres.read().rstrip('\n')
-brkset=set(res.replace('\n',','))
+brkset = set(res.replace('\n', ','))
 brkset.discard(',')
-#brkset={'6', '2', '5', '4', '3', '1'} #debugtest
-print ('在运行brkid:',brkset)
+# brkset={'6', '2', '5', '4', '3', '1'} #debugtest
+print('在运行brkid:', brkset)
 for i in brkid:
-    i=str(i)
-    if i not in brkset: 
-        print(i,'broker退服')
-
-# brokerlist = ''
-# with open('brokerid.txt', mode='r', encoding='utf8') as  f:
-#     brokerlist = f.read()
-# print(brokerlist)
+    i = str(i)
+    if i not in brkset:
+        print(i, 'broker退服')
