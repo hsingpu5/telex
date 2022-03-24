0af5f8295637626e17f92e65c22a270d01b9316f
hsingpu <1927833039@qq.com>
2022-03-24 16:16:42 +0800

    alert脚本修改

--- kfkCheck/bin/alter.py
+++ kfkCheck/bin/alter.py
@@ -2,12 +2,22 @@
 
 import requests  # pip3 install requests -i https://pypi.douban.com/simple
 import json
+import sys
 
+url = 'http://133.0.206.49:9093/api/v1/alerts'
 headers = {
     'content-type': 'application/json',
 }
 
-url = 'http://133.0.206.49:9093/api/v1/alerts'
+try:
+    ip = sys.argv[1]
+    # 脚本执行传入的参数  例如:来自zbx的shell脚本调用  传入第二个参数为报警标题 第三个为报警内容
+    summary = sys.argv[2]
+    description = sys.argv[3]
+    paasServiceName = sys.argv[4]
+except IndexError as e:
+    ip = 'without_ip'
+    description = 'without_description'
 
 
 def alterinfo(ip, summary, description, paasServiceName="CRM3-AMC"):
@@ -31,3 +41,9 @@ def alterinfo(ip, summary, description, paasServiceName="CRM3-AMC"):
     response = requests.post(url, headers=headers, json=data)
     print(response.content)
     return response.content
+
+
+if __name__ == '__main__':
+    # if ip:
+    #    print(ip, summary, description, paasServiceName)
+    alterinfo(ip, summary, description, paasServiceName)
