153f862edb3fe38f76de95f486010fa12e6457d2
hsingpu <1927833039@qq.com>
2022-04-02 17:13:21 +0800

    no message

--- es/demo.py
+++ es/demo.py
@@ -7,6 +7,7 @@ BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
 sys.path.insert(0, BASE_DIR)
 from conf import es_auth
 
+write_list = []
 # es连接信息
 
 es = Elasticsearch(["133.0.120.198:9200"], http_auth=es_auth)
@@ -44,7 +45,8 @@ def getindexname():
 
 
 def logfilename():
-    now = datetime.now().strftime('%Y.%m.%d--%H.%M.%S')
+    # now = datetime.now().strftime('%Y.%m.%d--%H.%M.%S')
+    now = datetime.now().strftime('%Y.%m.%d')
     return r'D:\code\telex\es\logs\\' + now + '.log'
 
 
@@ -52,19 +54,45 @@ def getinfo(indexname, body):
     '''执行查询'''
     # res = es.get(index=indexname, body=body)
     res = es.search(index=indexname, body=body)
+
     return json.dumps(res, ensure_ascii=False)
     # return res
 
 
 def writefile(info):
+    if not info: return
     res = logfilename()
-    with open(res, mode='w+', encoding='utf-8') as f:
-        f.write(info)
+    with open(res, mode='a', encoding='utf-8') as f:
+        for i in info:
+            f.write(str(i) + '\n')
 
 
-indexname = getindexname()  # 获取当前索引名
-res = getinfo(indexname, body)  # 传入索引名 及查询语句
-print(res)
-writefile(res)  # 写入文件
+def ana():
+    '''
+    解析查询到的文本内容
+    :return:
+    '''
+    k1 = 'apiName'
+    k2 = 'timestamp'
+    indexname = getindexname()  # 获取当前索引名
+    res = getinfo(indexname, body)  # 传入索引名 及查询语句
+    res = json.loads(res)
+    res = res.get("hits").get("hits")
+
+    for i in res:
+        # print('hits内容:', i)
+        info = i.get('_source').get(k1)
+        tm = i.get('_source').get(k2)
+        tm = str(tm)[0:10]
+        tm = datetime.fromtimestamp(int(tm))
+        tm = tm.strftime('%Y-%m-%d %H:%M:%S')
+        write_dic = {k2: tm, k1: info}
+        # print(write_dic)
+        write_list.append(write_dic)
+    return write_list
+
+
 if __name__ == '__main__':
-    pass
+    res = ana()
+    print(res)
+    writefile(res)  # 写入文件
