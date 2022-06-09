42f5f4a7eb4ca403abed45295e6ef4e2ca68647f
hsingpu <1927833039@qq.com>
2022-06-09 10:51:48 +0800

    TESTFILE

---
+++ o2pg/pg.sql
@@ -0,0 +1,21 @@
+create table table_name
+(
+    id          int4(12),
+    text        text (255 CHAR) not null, --char类型,一个汉字占一个长度
+    PID         text (32 BYTE) NOT NULL,  --byte类型,UTF8一个汉字占大约两个长度
+    status      int4(1) DEFAULT 0 null,    --添加默认值 如果为空默认值就为0
+    phoneNumber int4(11),
+    cash        float4(6),
+    info        timestamp (12)
+)
+--添加主键
+ALTER TABLE "test"."table_name"
+    ADD PRIMARY KEY ("ID");
+
+--添加注释
+comment
+on column table_name.id is '主键';
+    comment
+on column table_name.text is '说明';
+    comment
+on column table_name.status is '状态';
